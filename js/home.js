abf549d812d83db659ee812f5bda5eb47df0c043
barua-raj <barua.jovraj@gmail.com>
2025-08-27 21:26:21 +0600

    toggle and cash out feature done but need to work on validation later

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
 const bankAccountNumber = 12345678912;
 const validPin = 1234;
+// add money feature
 document.getElementById('btn-add-money').addEventListener('click', function(e){
     e.preventDefault();
 
@@ -7,6 +8,7 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
     const accountNumber = parseInt(document.getElementById('account-number').value);
     const addAmount = parseInt(document.getElementById('add-amount').value);
     const pinNumber = parseInt(document.getElementById('add-pin').value);
+    const availableAmount = parseInt(document.getElementById('available-balance').innerText);
 
     if (accountNumber.length < 11) {
         alert("Please, provide a valid bank account number!");
@@ -23,8 +25,40 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
         return;
     }
 
-    const availableAmount = parseInt(document.getElementById('available-balance').innerText);
     const totalNewAvailableBalance = availableAmount + addAmount;
     document.getElementById('available-balance').innerText = totalNewAvailableBalance;
 
+})
+// cash out money
+document.getElementById('btn-withdraw').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const agentNumber = document.getElementById('agent-number').value;
+    const amountWithdraw = parseInt(document.getElementById('withdraw-amount').value);
+    const availableAmount = parseInt(document.getElementById('available-balance').innerText);
+    const pinNumber = parseInt(document.getElementById('add-pin').value);
+    
+    if (agentNumber.length > 11) {
+        alert('Please, provide a valid agent number!');
+        return;
+    }
+
+    if (validPin !== pinNumber) {
+        alert("Please, provide a valid pin number!");
+        return;
+    }
+    const totalNewAvailableBalance = availableAmount - amountWithdraw;
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance;
+    
+    
+})
+// toggle feature
+
+document.getElementById('add-money-btn').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = "none";
+    document.getElementById('add-money-parent').style.display = "block";
+})
+document.getElementById('cash-out-btn').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = "none";
+    document.getElementById('cash-out-parent').style.display = "block";
 })
